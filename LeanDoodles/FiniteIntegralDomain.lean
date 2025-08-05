35e7bb7fc4630f79a1153b9470c20c0b1fd0188d
Michał Dobranowski <mdbrnowski@gmail.com>
2025-08-06 00:21:32 +0200

    Every finite integral domain is a field (#3)
    Every finite integral domain is a field. Special case of
    `Finite.isDomain_to_isField`.

---
+++ LeanDoodles/FiniteIntegralDomain.lean
@@ -0,0 +1,27 @@
+/-
+Copyright (c) 2025 Michał Dobranowski. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Michał Dobranowski
+-/
+import Mathlib.Algebra.Ring.Defs
+import Mathlib.Algebra.Field.IsField
+import Mathlib.Data.Finite.Card
+
+open Function
+
+/-- Every finite integral domain is a field. -/
+theorem finite_integral_domain_is_field (R : Type*) [CommRing R] [IsDomain R] [Finite R] :
+    IsField R := by
+  constructor
+  · -- Fields have two distinct elements.
+    exact IsDomain.mk.exists_pair_ne
+  · -- Fields are commutative.
+    exact CommRing.mul_comm
+  · -- Nonzero elements have multiplicative inverses.
+    intro a ha
+    let f : R → R := fun x => a * x
+    have h_inj : Injective f :=
+      fun x y h => mul_left_cancel₀ ha (h : f x = f y)
+    have h_surj : Surjective f :=
+      Finite.injective_iff_surjective.mp h_inj
+    exact h_surj 1
